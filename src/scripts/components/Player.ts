e590128d996aed32e4d71602679996e4d8ff6cb9
Timur Ermolaev <timkogas@gmail.com>
2023-05-26 11:51:38 +0300

    change positions for player

--- src/scripts/components/Player.ts
+++ src/scripts/components/Player.ts
@@ -1,7 +1,8 @@
 import Game from '../scenes/Game';
 
 const MARGIN_X = 150
-const MARGIN_X_PLAYER = 330
+const MARGIN_X_PLAYER_SPRITE = 330
+const MARGIN_Y_PLAYER_SPRITE  = 100
 
 class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Game) {
@@ -22,8 +23,10 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
     this._scene.add.existing(this);
     this._scene.physics.add.existing(this);
-    this._sprite = this._scene.add.sprite(this.x + MARGIN_X_PLAYER, this._scene.actions.woodElements.leftUp.getBounds().bottom + 200, 'player-down')
+    this._sprite = this._scene.add.sprite(this.x + MARGIN_X_PLAYER_SPRITE, this._scene.scale.height - MARGIN_Y_PLAYER_SPRITE, 'player-down')
     this._sprite.flipX = true
+    this._sprite.setOrigin(0.5, 1)
+    this._sprite.setScale(1, 1.2)
     this.setBodySize(280, 250)
     this.setAlpha(0)
     this.setDepth(12)
@@ -31,13 +34,13 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
   public right(): void {
     this.setPosition(this._scene.actions.woodElements.rightDown.getBounds().left - MARGIN_X, this.y)
-    this._sprite.setPosition(this.x - MARGIN_X_PLAYER, this._scene.actions.woodElements.leftUp.getBounds().bottom + 200)
+    this._sprite.setPosition(this.x - MARGIN_X_PLAYER_SPRITE, this._scene.scale.height - MARGIN_Y_PLAYER_SPRITE)
     this._sprite.flipX = false
   }
 
   public left(): void {
     this.setPosition(this._scene.actions.woodElements.leftDown.getBounds().right + MARGIN_X, this.y)
-    this._sprite.setPosition(this.x + MARGIN_X_PLAYER, this._scene.actions.woodElements.leftUp.getBounds().bottom + 200)
+    this._sprite.setPosition(this.x + MARGIN_X_PLAYER_SPRITE, this._scene.scale.height - MARGIN_Y_PLAYER_SPRITE)
     this._sprite.flipX = true
   }
 
