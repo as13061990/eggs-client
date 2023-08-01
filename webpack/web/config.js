4a45cdeb6247cd154ceca8b42e9c4a0a9818590f
Timur Ermolaev <timkogas@gmail.com>
2023-08-01 16:37:15 +0300

    added diff builds

--- webpack/web/config.js
+++ webpack/web/config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   entry: './src/scripts/index.ts',
   output: {
-    path: path.resolve(__dirname, '../build'),
+    path: path.resolve(__dirname, '../../build'),
   },
   mode: "development",
   devtool: "eval-source-map",
@@ -49,7 +49,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      root: path.resolve(__dirname, "../../")
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
